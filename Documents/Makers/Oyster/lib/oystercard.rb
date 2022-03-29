d0aa2f47c9dfcef490e7a18787de043217e0b493
nadia <nadiabenzineb1@gmail.com>
2022-03-29 17:12:28 +0100

    Challenge 11 complete

--- Documents/Makers/Oyster/lib/oystercard.rb
+++ Documents/Makers/Oyster/lib/oystercard.rb
@@ -2,12 +2,12 @@ LIMIT = 90
 MIN_FARE = 1
 
 class Oystercard
-attr_reader :balance
+attr_reader :balance, :entry_station
 attr_accessor :in_journey, :balance
 
   def initialize
   @balance = 1
-  @in_journey = true
+  
   end
 
   def top_up(money)
@@ -18,21 +18,26 @@ attr_accessor :in_journey, :balance
   end
 
   def deduct(money)
-    @balance -= money
+    @balance = balance - money
   end
 
-  def touch_in
+  def touch_in(station)
+    @entry_station = station
     fail "There are insufficient funds" if balance < MIN_FARE
-    @injourney = true
+    in_journey?
   end
 
   def touch_out
     deduct(MIN_FARE)
-    @injourney = false
+    @entry_station = nil
+    in_journey?
   end
 
   def in_journey?
-    @injourney = true
+    if @entry_station == nil
+    false
+    else true
+    end
   end
 end
 
--- Documents/Makers/Oyster/spec/oystercard_spec.rb
+++ Documents/Makers/Oyster/spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/oystercard.rb'
 
 describe Oystercard do
+    let(:station){double :station}
+ 
   it "shows user's balance"  do
     expect(subject.balance).to eq subject.balance
   end
@@ -13,27 +15,36 @@ describe Oystercard do
   end
   it "deducts the fare from the balance" do
     allow(subject).to receive(:balance).and_return 5
-    p subject.balance
-    p MIN_FARE
-    expect(subject.deduct(MIN_FARE)).to eq 0
+    expect(subject.deduct(MIN_FARE)).to eq 4
   end
   it "should touch in" do 
-    subject.top_up(2)
-    expect(subject.touch_in).to eq true
+   subject.top_up(2)
+    expect(subject.touch_in(station)).to eq true
   end
   it "should touch out" do
- 
-   expect(subject).to receive(:touch_out)
+    subject.touch_in(station)
+    
+  expect( subject.touch_out).to eq false
   end
   it "the oyster card should be in use" do
+    subject.touch_in(station)
     expect(subject.in_journey?).to eq true
   end
   it "should raise an error if the user tries to touch in with insufficient funds" do
     allow(subject).to receive(:balance).and_return 0
-    expect{subject.touch_in}.to raise_error "There are insufficient funds"
+    expect{subject.touch_in(station)}.to raise_error "There are insufficient funds"
   end
   it "should deduct money from balance when touching out" do
-    subject.touch_in
+    subject.touch_in(station)
     expect {subject.touch_out}.to change{subject.balance}.by(-MIN_FARE)
+  end
+  it "should save entry station" do
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq station
+  end
+  it "should have no entry station saved on touch out" do
+    subject.touch_in(station)
+    subject.touch_out
+    expect(subject.entry_station).to be_nil
   end
 end
